14ade665bc5b70d8418fb35ed47753a2cbb14d7f
Yuvisa QP <yuvisa.palomino@gmail.com>
2022-02-02 00:10:05 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>"Mi primera aplicación"</h1>
+      <h1>Mi primera aplicación</h1>
       <p>
-      "Esta es mi primera aplicación en React, está padrísimo!".
+      Esta es mi primera aplicación en React, está padrísimo!
       </p>
     </div>
   );

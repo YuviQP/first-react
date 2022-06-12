d59fa7a85f0980dd29f8aca0e83b24d381addcc9
Yuvisa QP <yuvisa.palomino@gmail.com>
2022-06-11 20:36:01 -0500

    added some changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function App() {
   const [count,setCount]=useState(0);
   return (
-    <div className="App">
+    <div>
     <p>{count}</p>
     <button onClick={()=>setCount(count+1)}>Incrementar</button>
     </div>
